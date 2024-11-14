6dab24b9cde32b5ba31603749e4cdfdc9350c212
imanoari <101164506+imanoari@users.noreply.github.com>
2024-11-14 14:41:48 +0900

    PlanBelongingsControllerの作成

---
+++ src/main/java/com/tabisketch/controller/PlanBelongingsController.java
@@ -0,0 +1,15 @@
+package com.tabisketch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("plan/belongings/{uuid}")
+public class PlanBelongingsController {
+    @GetMapping()
+    public String planBelongings(@PathVariable("uuid") String uuid) {
+        return "plan/belongings";
+    }
+}
