5f11c3e9b04877faff1e5333d90f3c03fa2e5e79
imanoari <101164506+imanoari@users.noreply.github.com>
2024-11-28 14:11:53 +0900

    空のコントローラー作成

---
+++ src/main/java/com/tabisketch/controller/PasswordResetController.java
@@ -0,0 +1,14 @@
+package com.tabisketch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class PasswordResetController {
+    @GetMapping()
+    public String passwordReset() {
+        return "";
+    }
+}
