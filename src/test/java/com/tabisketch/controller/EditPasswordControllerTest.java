9b5092a802ce65da594a2f2aa9153d725a1eb482
imanoari <101164506+imanoari@users.noreply.github.com>
2024-12-18 14:20:58 +0900

    controllerのテストを作成

---
+++ src/test/java/com/tabisketch/controller/EditPasswordControllerTest.java
@@ -0,0 +1,37 @@
+package com.tabisketch.controller;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.stream.Stream;
+
+@WebMvcTest(EditPasswordController.class)
+public class EditPasswordControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @ParameterizedTest
+    @MethodSource("sampleMailAddress")
+    @WithMockUser(username = "sample@example.com")
+    public void getが動作するか(final String mailAddress) throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/user/edit"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("mailAddress"))
+                .andExpect(MockMvcResultMatchers.model().attribute("mailAddress", mailAddress))
+                .andExpect(MockMvcResultMatchers.view().name("user/edit/index"));
+    }
+
+    private static Stream<String> sampleMailAddress() {
+        final var mailAddress = "sample@example.com";
+        return Stream.of(mailAddress);
+    }
+}
+
+
